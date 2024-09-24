1f29f5f94b5573faa69f7ca83636add78596f012
Shan <shannu1st@gmail.com>
2024-09-24 10:53:52 +0530

    server commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Helper function for API requests
+async function makeApiRequest(url) {
+  try {
+    const response = await axios.get(url);
+    return {
+      status: 200,
+      success: true,
+      message: "Successfully fetched the data",
+      data: response.data,
+    };
+  } catch (error) {
+    console.error("API request error:", error.response ? error.response.data : error);
+    return {
+      status: 500,
+      success: false,
+      message: "Failed to fetch data from the API",
+      error: error.response ? error.response.data : error.message,
+    };
+  }
+}
+
+app.get("/stock-market", async (req, res) => {
+  //let pageSize = parseInt(req.query.pageSize) || 80;
+  let page = parseInt(req.query.page) || 1;
+  let url = `https://api.marketaux.com/v1/news/all?symbols=TSLA%2CAMZN%2CMSFT&filter_entities=true&language=en&page=${page}&api_token=${process.env.API_KEY}`;
+  const result = await makeApiRequest(url);
+  res.status(result.status).json(result);
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Server is running at port ${PORT}`);
+});
+
